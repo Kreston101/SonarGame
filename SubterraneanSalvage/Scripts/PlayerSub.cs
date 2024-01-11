03b23d38fd75f8ae922e7e4521bd174de9ae0eda
Kreston101 <93973537+Kreston101@users.noreply.github.com>
2024-01-11 15:51:33 +0800

    basic ping working
    aka nothing fancy installed

--- SubterraneanSalvage/Scripts/PlayerSub.cs
+++ SubterraneanSalvage/Scripts/PlayerSub.cs
@@ -4,16 +4,26 @@ using System;
 public partial class PlayerSub : CharacterBody2D
 {
 	public float speed = 300.0f;
+	[Export] public float speedMultiplier = 1f;
 
 	private Node2D subBody;
+	private Node2D rayCasts;
+	private Node2D indicators;
+
+	private float timer = 0;
 
 	public override void _Ready()
 	{
 		subBody = (Node2D)GetNode("SubBody");
+		rayCasts = (Node2D)GetNode("Raycasts");
+		indicators = (Node2D)GetNode("Indicators");
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		timer += (float)delta;
+
+		//input control
 		Vector2 velocity = new Vector2();
 		if (Input.IsActionPressed("Left"))
 		{
@@ -34,11 +44,67 @@ public partial class PlayerSub : CharacterBody2D
 			velocity.Y = 1;
 		}
 
-		Position += velocity * speed * (float)delta;
-
-		if(velocity == Vector2.Zero)
+		//speed control, faster is louder
+		if (Input.IsActionJustPressed("Speed+"))
 		{
-			RotationDegrees = 0;
+			if(speedMultiplier < 1.5f)
+			{
+				speedMultiplier += 0.5f;
+			}
+			GD.Print(speedMultiplier);
+		}
+		if (Input.IsActionJustPressed("Speed-"))
+		{
+			if (speedMultiplier > 0.5f)
+			{
+				speedMultiplier -= 0.5f;
+			}
+			GD.Print(speedMultiplier);
+		}
+
+		Position += velocity.Normalized() * speed * speedMultiplier * (float)delta;
+
+		//pings around the sub, checking for nearby walls/objs/creatures
+		if (Input.IsActionJustPressed("Ping"))
+		{
+			SubPing();
+		}
+
+		MoveAndSlide();
+
+		float limit = 2f;
+		if (timer >= limit)
+		{
+			GD.Print("ran");
+			ExpirePing();
+			timer = 0;
+		}
+	}
+
+	public void SubPing()
+	{
+		RayCast2D ray;
+		Node2D indicatorPing;
+		for(int _i = 0;  _i <= rayCasts.GetChildCount() - 1; _i++)
+		{
+			ray = (RayCast2D)rayCasts.GetChild(_i);
+			indicatorPing = (Node2D)indicators.GetChild(_i);
+			if (ray.IsColliding())
+			{
+				indicatorPing.Show();
+			}
+			else
+			{
+				indicatorPing.Hide();
+			}
+		}
+	}
+
+	public void ExpirePing()
+	{
+		foreach(Node2D child in indicators.GetChildren())
+		{
+			child.Hide();
 		}
 	}
 }
